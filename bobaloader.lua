c57a929442b7c15d38816da488ba4f4adfb77eec
inner <epicfied2.0@gmail.com>
2026-03-19 15:07:38 -0700

    Update bobaloader.lua

--- bobaloader.lua
+++ bobaloader.lua
@@ -1,30 +1,39 @@
-local loadver = ...
+local loadver = ({...})[1]
 local cloneref = cloneref or function(obj)
     return obj
 end
 local starterGui = cloneref(game:GetService('StarterGui'))
 
 local version = (loadver and tostring(loadver)) or 'main'
+local url = 'https://raw.githubusercontent.com/nolumn/Bobo/refs/heads/main/games/'..tostring(game.PlaceId)..'/'..version..'.lua'
 
 local suc, res = pcall(function()
-    return game:HttpGet('https://raw.githubusercontent.com/nolumn/Bobo/refs/heads/main/games/'..tostring(game.PlaceId)..'/'..version..'.lua')
+    return game:HttpGet(url)
 end)
 
-if suc and res and res ~= '' then
-    local runsuc, runres = pcall(function()
-        loadstring(res)()
-    end)
-    if not runsuc then
-        starterGui:SetCore('SendNotification', {
-            Title = 'Failed to load!',
-            Text = 'Error: '..tostring(runres),
-            Duration = 20
-        })
-    end
-else
+if not suc then
     starterGui:SetCore('SendNotification', {
-        Title = 'Failed to fetch!',
-        Text = 'Could not reach: '..tostring(game.PlaceId)..'/'..version,
+        Title = 'Fetch failed',
+        Text = tostring(res),
+        Duration = 20
+    })
+    return
+end
+
+if not res or res == '' or res:find('404') then
+    starterGui:SetCore('SendNotification', {
+        Title = 'File not found',
+        Text = url,
+        Duration = 20
+    })
+    return
+end
+
+local runsuc, runres = pcall(loadstring(res))
+if not runsuc then
+    starterGui:SetCore('SendNotification', {
+        Title = 'Run failed',
+        Text = tostring(runres),
         Duration = 20
     })
 end
